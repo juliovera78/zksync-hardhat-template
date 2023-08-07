3e0766ce6e7c538e10ce9d237385ad400dbc454e
Dustin Brickwood <dustinbrickwood204@gmail.com>
2023-08-07 16:09:07 -0500

    chore: test wagmi frontend with template and paymaster

--- contracts/Greeter.sol
+++ contracts/Greeter.sol
@@ -4,15 +4,21 @@ pragma solidity ^0.8.0;
 contract Greeter {
     string private greeting;
 
+    event Greeting(string greeting);
+    event GreetingChanged(string oldGreeting, string newGreeting);
+
     constructor(string memory _greeting) {
         greeting = _greeting;
     }
 
     function greet() public view returns (string memory) {
+        emit Greeting(_greeting);
         return greeting;
     }
 
     function setGreeting(string memory _greeting) public {
+        emit GreetingChanged(greeting, _greeting);
         greeting = _greeting;
     }
 }
+
